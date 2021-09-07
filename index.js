11b3e2ad0995638fdaa07b66962f1a78e45a73e6
onkarsarje <73869353+onkarsarje@users.noreply.github.com>
2021-09-07 16:39:51 +0530

    update: uploaded correct code from repl

--- index.js
+++ index.js
@@ -1,36 +1,86 @@
-const billAmount = document.querySelector("#bill-amount");
-const cashGiven = document.querySelector("#cash-given");
-const checkButton = document.querySelector("#check-button");
-const message = document.querySelector("#error-message");
-const noOfNotes = document.querySelectorAll(".no-of-notes");
+var readlineSync = require("readline-sync");
+const chalk = require('chalk');
+var score=0;
+var rightAnswerMessage=chalk.bold.green("Right!")
+var wrongAnswerMessage=chalk.bold.red("Wrong!")
+var userName = readlineSync.question("What's your name? ");
+console.log("Welcome "+chalk.magenta(userName.toUpperCase())+" to `DO YOU KNOW Onkar?`");
 
-const availableNotes = [2000, 500, 100, 20, 10, 5, 1];
+function play(question, answer){
+  var userAnswer= readlineSync.question(question);
 
-checkButton.addEventListener("click", function validateBillAndCashAmount() {
-    message.style.display = "none";
-    if (billAmount.value > 0) {
-        if (cashGiven.value >= billAmount.value) {
-            const amountToBeReturned = cashGiven.value - billAmount.value;
-            calculateChange(amountToBeReturned);
-        } else {
-            showMessage("Do you wanna wash plates?");
-        }
-    } else {
-        showMessage("Invalid Bill Amount");
-    }
-});
-
-function calculateChange(amountToBeReturned) {
-    for (let i = 0; i < availableNotes.length; i++) {
-        const numberOfNotes = Math.trunc( amountToBeReturned / availableNotes[i]
-        );
-        amountToBeReturned %= availableNotes[i];
-        noOfNotes[i].innerText = numberOfNotes;
-    }
+  if (userAnswer.toUpperCase()===answer.toUpperCase()){
+    console.log(rightAnswerMessage);
+    score++;
+    
+  }else{
+    console.log(wrongAnswerMessage);
+  }
+  console.log("Current Score: ",score);
+  console.log("---------------")
 }
 
-function showMessage(msg) {
-    message.style.display = "block";
-    message.innerText = msg;
+var highScores = [
+  {
+    name: "Onkar",
+    userHighScore: 10,
+  },
+  {
+    name: "Aadarsh",
+    userHighScore: 9,
+  },{
+    name: "Bhuvan",
+    userHighScore: 7,
+  }
+]
+
+var questions=[{
+  question: "Q1] Where do I live? ",
+  answer: "Solapur"
+},{
+  question: "2] My favourite superhero would be? ",
+  answer: "Wolverine"
+},{
+  question: "3] Where do I work? ",
+  answer: "Jio"
+},{
+  question: "Which Book am I currently reading? ",
+  answer: "Black Swan"
+},{
+  question: "Which vehicle did I drive to college on? ",
+  answer: "Activa"
+},{
+   question: "What is my Age? ",
+  answer: "21"
+},{
+   question: "Which School did I go to?  ",
+  answer: "IMS"
+},{
+   question: "Which Junior College did I go to? ",
+  answer: "Walchand"
+},{
+   question: "Which College did go to for Graduation? ",
+  answer: "Modern"
+},{
+   question: "What was my major in college? ",
+  answer: "computer"
+},{
+   question: "Which city was my College in? ",
+  answer: "pune"
+}]
+
+
+for (var i=0; i<questions.length; i++){
+  var currentQuestion=questions[i];
+  play(currentQuestion.question, currentQuestion.answer)
 }
-// Hey
+
+console.log("Yay! You Scored: ", chalk.magenta(score));
+
+for (var i=0; i<highScores.length; i++){
+  var currentHighScorer=highScores[i];
+  console.log(currentHighScorer.name);
+  console.log(currentHighScorer.userHighScore);
+}
+
+// console.log("Check out the Highest Scorer: ",highScores.Onkar)
